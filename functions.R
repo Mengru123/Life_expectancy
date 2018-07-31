a18df2d78f78b3912d61fec85f8ba21d4f050c7b
Mon <yuanmengru@gmail.com>
2018-07-30 21:06:12 -0400

    LE calculation

--- functions.R
+++ functions.R
@@ -113,9 +113,11 @@ create_LE = function(df1.rounte, df2.rounte,  max_age, yr1, yr2, yr3, n1, n2, n3
     df = join(df.num, df.denom, by = c("hr", "age_cat", "value"))
     names(df) = c("hr", "year", "age_cat","deaths", "pop" )
     df$rate = df$deaths/df$pop
+
     df = df[, c("hr", "year",  "age_cat", "rate", "pop")]
     df.sp = split(df, df$hr)
     rm(df.denom); rm(df.num); rm(df)
+    
     
     df.ori = join(df.ori.num, df.ori.denom, by = c("hr", "age", "year"))
     names(df.ori) = c("age", "year", "hr","deaths", "pop" )
@@ -153,4 +155,75 @@ create_LE = function(df1.rounte, df2.rounte,  max_age, yr1, yr2, yr3, n1, n2, n3
     
 }
 
+create_LE_1yr = function(df1.rounte, df2.rounte,  max_age, yr1, yr2, yr3, n1, n2, n3, max_age_cat, n_cat1, n_cat2max) {
+    df.denom = read.csv(as.character(df1.rounte), header = FALSE)
+    names(df.denom) = c("hr", "age", "count", "year")
+    df.denom$year = substr(df.denom$year, 1, 4)
+    df.num = read.csv(as.character(df2.rounte), header = FALSE) 
+    names(df.num) = c("age", "year", "hr", "count") # year --> death year
+    df.num = df.num[df.num$count != 0, ]
+    df.denom= as.data.frame(xtabs(df.denom$count  ~., df.denom))
+    df.num = as.data.frame(xtabs(df.num$count ~., df.num)) 
+    df.ori.denom = df.denom
+    df.ori.num = df.num
+    #set maximum age 
+    df.denom = set_max_age(df.denom, max_age)
+    df.num = set_max_age(df.num, max_age)
+    # group data by year groups 
+    df.denom = group_nconsecutive_yr(df.denom, yr1, yr2, yr3, n1,n2,n3)
+    df.num = group_nconsecutive_yr(df.num,yr1, yr2, yr3, n1,n2,n3)
+    
+    # Group age categories (0-4, 5-10, every 5-year age category, 90+)----------------------------------------------------
+    df.denom$age_cat = as.numeric(as.character(df.denom$age))+1 # if not as character then age0 will disappear
+    df.denom = df.denom[, c("hr", "value", "age_cat", "Freq")]
+    df.denom = as.data.frame(xtabs(df.denom$Freq ~., df.denom))
+    
+    df.num$age_cat = as.numeric(as.character(df.num$age))+1
+    df.num = df.num[, c("hr", "value", "age_cat", "Freq")]
+    df.num = as.data.frame(xtabs(df.num$Freq ~., df.num))
+    
+    
+    df = join(df.num, df.denom, by = c("hr", "age_cat", "value"))
+    names(df) = c("hr", "year", "age_cat","deaths", "pop" )
+    df$rate = df$deaths/df$pop
+    df = df[, c("hr", "year",  "age_cat", "rate", "pop")]
+    df.sp = split(df, df$hr)
+    rm(df.denom); rm(df.num); rm(df)
+    
+    
+    df.ori = join(df.ori.num, df.ori.denom, by = c("hr", "age", "year"))
+    names(df.ori) = c("age", "year", "hr","deaths", "pop" )
+    df.ori.sp = split(df.ori, df.ori$hr)
+    rm(df.ori);rm(df.ori.denom)
+    
+    # create life table component ------------------------------------------------------
+    # calculate the death probabilites
+    t= lapply(df.ori.sp, function(x) calculate_constantC(x))
+    rm(df.ori.num);rm(df.ori.sp)
+    df.sp = add_constantC(df.sp, t);rm(t)
+    df.sp = lapply(df.sp, function(x) add_death_prob(x, max_age_cat, n_cat1, n_cat2max))
+    
+    # calculate life table components based on fictitious cohort of 100,000 newborns
+    df.sp = lapply(df.sp, function(x) split(x, x$year))
+
+    df.sp = lapply(df.sp, function(x) {
+        lapply(x, function(x){
+            life_table_comp(x, max_age_cat, n_cat1, n_cat2max)
+        })
+    })
+    
+    LE = lapply(df.sp, function(x) {
+        lapply(x, function(x){
+            T= sum(x$num_live)
+            le= T/100000
+            return(le)
+        })
+    })
+    
+    # LE table for all rss/year combinations
+    df = as.data.frame(matrix(unlist(LE), nrow=length(unlist(LE[1])), dimnames = list(NULL, names(LE))), row.names = names(LE[[1]]))
+    rm(df.sp); rm(LE)
+    return(df)
+    
+}
 
--- main.R
+++ main.R
@@ -24,5 +24,48 @@ df.clsc = create_LE(df1.rounte = "Data/1. CLSC/pophr_public_le_table_denom.csv",
                    max_age = 90, yr1 = 2006, yr2 = 2011, yr3 = 2014, n1 = 9, n2 = 10, n3 =10, 
                    max_age_cat = 9, n_cat1 = 20, n_cat2max = 10)
 
-write.csv(df.rss, file = "C:/Users/Mengru/df.rss.csv")
-write.csv(df.clsc, file = "C:/Users/Mengru/df.clsc.csv")
+# write.csv(df.rss, file = "Output/LE_RSS.csv")
+# write.csv(df.clsc, file = "Output/LE_CLSA.csv")
+# write.csv(df.rls, file = "Output/LE_RLS.csv")
+
+df.rls.3yr = create_LE(df1.rounte = "Data/3. RLS/pophr_public_le_table_denom.csv", 
+                            df2.rounte = "Data/3. RLS/pophr_public_le_table_num.csv", 
+                            max_age = 90, yr1 = 2006, yr2 = 2011, yr3 = 2014, n1 = 3, n2 = 3, n3 =3, 
+                            max_age_cat = 10, n_cat1 = 10, n_cat2max = 10)
+
+
+df.rss.3yr = create_LE(df1.rounte = "Data/2. RSS/pophr_public_le_table_denom.csv", 
+                           df2.rounte = "Data/2. RSS/pophr_public_le_table_num.csv", 
+                           max_age = 90, yr1 = 2006, yr2 = 2011, yr3 = 2014, n1 = 3, n2 = 3, n3 =3,
+                           max_age_cat = 19, n_cat1 = 5, n_cat2max = 5)
+
+
+df.clsc.3yr = create_LE(df1.rounte = "Data/1. CLSC/pophr_public_le_table_denom.csv", 
+                             df2.rounte = "Data/1. CLSC/pophr_public_le_table_num.csv", 
+                             max_age = 90, yr1 = 2006, yr2 = 2011, yr3 = 2014, n1 = 3, n2 = 3, n3 =3, 
+                             max_age_cat = 9, n_cat1 = 20, n_cat2max = 10)
+
+df.rls.comp = create_LE_1yr(df1.rounte = "Data/3. RLS/pophr_public_le_table_denom.csv", 
+                   df2.rounte = "Data/3. RLS/pophr_public_le_table_num.csv", 
+                   max_age = 90, yr1 = 2006, yr2 = 2011, yr3 = 2014, n1 = 3, n2 = 3, n3 =3, 
+                   max_age_cat = 91, n_cat1 = 1, n_cat2max = 1)
+
+
+df.rss.comp = create_LE_1yr(df1.rounte = "Data/2. RSS/pophr_public_le_table_denom.csv", 
+                   df2.rounte = "Data/2. RSS/pophr_public_le_table_num.csv", 
+                   max_age = 90, yr1 = 2006, yr2 = 2011, yr3 = 2014, n1 = 3, n2 = 3, n3 =3,
+                   max_age_cat = 91, n_cat1 = 1, n_cat2max = 1)
+
+
+df.clsc.comp = create_LE_1yr(df1.rounte = "Data/1. CLSC/pophr_public_le_table_denom.csv", 
+                    df2.rounte = "Data/1. CLSC/pophr_public_le_table_num.csv", 
+                    max_age = 90, yr1 = 2006, yr2 = 2011, yr3 = 2014, n1 = 3, n2 = 3, n3 =3, 
+                    max_age_cat = 91, n_cat1 = 1, n_cat2max = 1)
+
+
+# t= rbind(df.clsc, df.clsc.3yr, df.clsc.comp)
+# write.csv(t, "C:/Users/Mengru/Desktop/clsc_3mtd.csv")
+# t= rbind(df.rls, df.rls.3yr, df.rls.comp)
+# write.csv(t, "C:/Users/Mengru/Desktop/rls_3mtd.csv")
+# t= rbind(df.rss, df.rss.3yr, df.rss.comp)
+# write.csv(t, "C:/Users/Mengru/Desktop/rss_3mtd.csv")
